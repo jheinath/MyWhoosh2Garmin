607945ca6bce531c7f1b103af00c24a6f5dd5c25
Jay Queue <jonathan.quartier@gmail.com>
2024-11-20 11:37:58 +0100

    Added filedialog to set backup folder for FIT files

--- myWhoosh2Garmin.py
+++ myWhoosh2Garmin.py
@@ -15,9 +15,11 @@ Credits:        Garth by matin - for authenticating and uploading with
                 https://github.com/embeddedc/mw2gc
 """
 import os
+import json
 import subprocess
 import sys
 import logging
+import tkinter as tk
 from datetime import datetime
 from getpass import getpass
 from pathlib import Path
@@ -29,6 +31,7 @@ file_handler = logging.FileHandler('myWhoosh2Garmin.log')
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+
 
 def get_pip_command():
     """
@@ -44,9 +47,10 @@ def get_pip_command():
             stdout=subprocess.PIPE, 
             stderr=subprocess.PIPE
         )
-        return [sys.executable, "-m", "pip"]  # pip is available
+        return [sys.executable, "-m", "pip"]  
     except subprocess.CalledProcessError:
-        return None  # pip is not available
+        return None
+
 
 def install_package(package):
     """
@@ -72,6 +76,7 @@ def install_package(package):
     else:
         logger.info("pip is not available. Unable to install packages.")
 
+
 def ensure_packages():
     """
     Ensure all required packages are installed. If a package is missing,
@@ -83,12 +88,12 @@ def ensure_packages():
     required_packages = [
         "garth",
         "fit_tool",
+        "tkinter",
     ]
     for package in required_packages:
         if not importlib.util.find_spec(package):
             print(f"Installing missing package: {package}")
             install_package(package)
-
 
 import garth
 from garth.exc import GarthException, GarthHTTPError
@@ -102,8 +107,9 @@ from fit_tool.profile.messages.record_message import (
 from fit_tool.profile.messages.session_message import SessionMessage
 from fit_tool.profile.messages.lap_message import LapMessage
 
+
 TOKENS_PATH = Path(".garth")
-BACKUP_FOLDER = Path("MyWhooshFitBackup")
+FILE_DIALOG_TITLE = "MyWhoosh2Garmin"
 MYWHOOSH_PREFIX_WINDOWS = "MyWhooshTechnologyService.MyWhoosh_"
 
 
@@ -131,7 +137,7 @@ def get_fitfile_location() -> Path:
             / "MyWhoosh"
             / "Content"
             / "Data"
-         )
+        )
         if target_path.is_dir():
             return target_path
         else:
@@ -162,7 +168,43 @@ def get_fitfile_location() -> Path:
         return None
 
 
+def get_backup_path(json_file='backup_path.json') -> Path:
+    """
+    This function checks if a backup path already exists in a JSON file.
+    If it does, it returns the stored path. If the file does not exist, 
+    it prompts the user to select a directory via a file dialog, saves 
+    the selected path to the JSON file, and returns it.
+
+    Args:
+        json_file (str): Path to the JSON file containing the backup path.
+
+    Returns:
+        str or None: The selected backup path or None if no path was selected.
+    """
+    if os.path.exists(json_file):
+        with open(json_file, 'r') as f:
+            backup_path = json.load(f).get('backup_path')
+        if backup_path and os.path.isdir(backup_path):
+            logger.info(f"Using backup path from JSON: {backup_path}")
+            return Path(backup_path)
+        else:
+            logger.error("Invalid backup path stored in JSON.")
+            sys.exit(1)
+    else:
+        root = tk.Tk()
+        root.withdraw() 
+        backup_path = filedialog.askdirectory(title=f"Select {FILE_DIALOG_TITLE} Directory")
+        if not backup_path:
+            logger.info("No directory selected, exiting.")
+            return None
+        with open(json_file, 'w') as f:
+            json.dump({'backup_path': backup_path}, f)
+        logger.info(f"Backup path saved to {json_file}")
+    return Path(backup_path)
+
+
 FITFILE_LOCATION = get_fitfile_location()
+BACKUP_FITFILE_LOCATION = get_backup_path()
 
 
 def get_credentials_for_garmin():
@@ -201,16 +243,13 @@ def authenticate_to_garmin():
     """
     try:
         if TOKENS_PATH.exists():
-            # Resume session if tokens file exists
             garth.resume(".garth")
             try:
-                # Verify if the session is still valid
                 logger.info(f"Authenticated as: {garth.client.username}")
             except GarthException:
                 logger.info("Session expired. Re-authenticating...")
                 get_credentials_for_garmin()
         else:
-            # No session tokens; ask for credentials
             logger.info("No existing session. Please log in.")
             get_credentials_for_garmin()
     except GarthException as e:
@@ -236,14 +275,12 @@ def cleanup_fit_file(fit_file_path: Path, new_file_path: Path) -> None:
     power_values = []
     heart_rate_values = []
 
-    # Process the records
     for record in fit_file.records:
         message = record.message
-        # Skip invalid records based on type and remove unnecessary fields
         if isinstance(message, (FileCreatorMessage, LapMessage)):
             continue
         if isinstance(message, RecordMessage):
-            message.remove_field(RecordTemperatureField.ID)  # Remove temp
+            message.remove_field(RecordTemperatureField.ID)
             cadence_values.append(message.cadence 
                                   if message.cadence 
                                   else 0)
@@ -254,7 +291,6 @@ def cleanup_fit_file(fit_file_path: Path, new_file_path: Path) -> None:
                                      if message.heart_rate 
                                      else 0)
         if isinstance(message, SessionMessage):
-            # Add average values if missing
             if not message.avg_cadence:
                 message.avg_cadence = (
                     sum(cadence_values) / len(cadence_values)
@@ -283,7 +319,6 @@ def cleanup_fit_file(fit_file_path: Path, new_file_path: Path) -> None:
             power_values = []
             heart_rate_values = []
         builder.add(message)
-
     out_file = builder.build()
     out_file.to_file(str(new_file_path))
     logger.info(f"Cleaned-up file saved as {new_file_path.name}")
@@ -303,22 +338,22 @@ def cleanup_and_save_fit_file(fitfile_location: Path) -> Path:
     """
     if fitfile_location.is_dir():
         logger.debug(f"Checking for .fit files in directory: "
-                     "{fitfile_location}")
-        # Find all .fit files in the directory
+                     f"{fitfile_location}")
         fit_files = list(fitfile_location.glob("*.fit"))
         if fit_files:
             logger.debug("Found the following .fit files:")
             fit_file = max(fit_files, key=lambda f: f.stat().st_mtime)
             timestamp = datetime.now().strftime("%Y-%m-%d_%H%M%S")
             new_filename = f"{fit_file.stem}_{timestamp}.fit"
-            if not BACKUP_FOLDER.exists():
-                BACKUP_FOLDER.mkdir()
-            new_file_path = BACKUP_FOLDER / new_filename
+            if not BACKUP_FITFILE_LOCATION.exists():
+                logger.error(f"{BACKUP_FITFILE_LOCATION} does not exist. "
+                             "Did you delete it?")
+            new_file_path = BACKUP_FITFILE_LOCATION / new_filename
             logger.info(f"Cleaning up {new_file_path}")
             try:
                 cleanup_fit_file(fit_file, new_file_path)  
                 logger.info(f"Successfully cleaned {fit_file.name}" 
-                            "and saved the file as {new_file_path.name}.")
+                            f"and saved the file as {new_file_path.name}.")
                 return new_file_path
             except Exception as e:
                 logger.error(f"Failed to process {fit_file.name}: {e}")
@@ -327,7 +362,7 @@ def cleanup_and_save_fit_file(fitfile_location: Path) -> Path:
             return Path()
     else:
         logger.info(f"The specified path is not a directory:" 
-                    "{fitfile_location}")
+                    f"{fitfile_location}")
         return Path()
 
 
@@ -366,6 +401,7 @@ def main():
     new_file_path = cleanup_and_save_fit_file(FITFILE_LOCATION)
     if new_file_path:
         upload_fit_file_to_garmin(new_file_path)
+
 
 if __name__ == "__main__":
     main()
